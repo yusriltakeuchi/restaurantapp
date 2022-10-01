f9252606717664665dafc8eb0344489aadb48f7e
Yusril Rapsanjani <yusriltakeuchi@gmail.com>
2022-10-01 21:26:43 +0700

    Feat:  Add precache local assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'dart:convert';
 import 'dart:io';
-
 import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 import 'package:restaurantapp/core/utils/background/backgorund_service_utils.dart';
@@ -45,16 +47,52 @@ void main() async {
   ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   final List<dynamic> providers;
   const MyApp({Key? key, 
     required this.providers,
   }) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  void precacheAssets() async {
+    final manifestJson = await rootBundle.loadString('AssetManifest.json');
+    final rawAssets = json.decode(manifestJson) as Map<String, dynamic>;
+    final images = rawAssets.keys.where((String key) => key.startsWith('assets/images')).toList();
+    final icons = rawAssets.keys.where((String key) => key.startsWith('assets/icons')).toList();
+    List<String> assets = [];
+    assets.addAll(images);
+    assets.addAll(icons);
+
+    for (var asset in assets) {
+      try {
+        if (asset.contains(".png")) {
+          // ignore: use_build_context_synchronously
+          precacheImage(AssetImage(asset), context);
+        } else if (asset.contains(".svg") && asset != "assets/icons/icon_direction.svg") {
+          precachePicture(ExactAssetPicture(SvgPicture.svgStringDecoderBuilder, asset), null);
+        }
+      } catch(e) {
+        debugPrint("ERROR: $e");
+        continue;
+      }
+    }
+  }
+
+  @override
+  void didChangeDependencies() {
+    precacheAssets();
+    super.didChangeDependencies();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: providers as List<SingleChildWidget>,
+      providers: widget.providers as List<SingleChildWidget>,
       child: MaterialApp(
         title: 'Restaurant App',
         navigatorKey: locator<NavigationUtils>().navigatorKey,
